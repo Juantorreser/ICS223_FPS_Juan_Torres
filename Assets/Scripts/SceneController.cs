16fe12c8ee9644b113d0316b09c79aac0db2bd2e
Juantorreser <jdtorreserazo@gmail.com>
2025-03-21 15:27:01 -0700

    Lab 7 - Completed

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class SceneController : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
-    [SerializeField] private int enemyCount = 5; 
+    [SerializeField] private GameObject iguanaPrefab;
+    [SerializeField] private int enemyCount = 5;
+    [SerializeField] private Transform iguanaSpawnPt;
     private GameObject[] enemies; 
     private Vector3 spawnAreaMin = new Vector3(-5, 0, -5);
     private Vector3 spawnAreaMax = new Vector3(5, 0, 5);
+    [SerializeField] private int iguanasCount = 5;
+    private GameObject[] iguanas;
 
     void Start()
     {
         enemies = new GameObject[enemyCount]; // Initialize array
         SpawnEnemies(); // Spawn initial enemies
+        iguanas = new GameObject[iguanasCount]; // Initialize array
+        SpawnIguanas(); // Spawn initial enemies
     }
 
     void Update()
@@ -34,6 +40,14 @@ public class SceneController : MonoBehaviour
         }
     }
 
+    void SpawnIguanas()
+    {
+        for (int i = 0; i < iguanasCount; i++)
+        {
+            iguanas[i] = SpawnIguana();
+        }
+    }
+
     GameObject SpawnEnemy()
     {
         Vector3 spawnPos = new Vector3(
@@ -46,4 +60,18 @@ public class SceneController : MonoBehaviour
         newEnemy.transform.Rotate(0, Random.Range(0, 360), 0);
         return newEnemy;
     }
+
+    GameObject SpawnIguana()
+    {
+        Vector3 spawnPos = iguanaSpawnPt.position + new Vector3(
+            Random.Range(-2f, 2f), // Small random offset in X
+            0,
+            Random.Range(-2f, 2f)  // Small random offset in Z
+        );
+
+        GameObject newIguana = Instantiate(iguanaPrefab, spawnPos, Quaternion.identity);
+        newIguana.transform.Rotate(0, Random.Range(0, 360), 0); // Random rotation
+        return newIguana;
+    }
+
 }
